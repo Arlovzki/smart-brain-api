4c3fb0a2beaccdc1470da66ecdacb800ddc21e90
Arlovzki <arloubeloria@gmail.com>
2020-06-20 01:14:49 +0800

    changing post to get of default route

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/', (req, res) => {res.send('it is working!')})
+app.get('/', (req, res) => {res.send('it is working!')})
 
 app.post('/signin', (req, res) => {signIn.handlesignIn(req, res, db, bcrypt)})
 

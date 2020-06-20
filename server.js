eb1fb3735c5f7460b95eb70d5fb62e776a4332c8
Arlovzki <arloubeloria@gmail.com>
2020-06-20 15:28:13 +0800

    changing database info

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require ('./controllers/image');
 const db= knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : '050498',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl:true
     }
   });
 
